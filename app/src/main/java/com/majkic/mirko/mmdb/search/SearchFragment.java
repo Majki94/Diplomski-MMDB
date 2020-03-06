585aad2fddb46745befd03c838cdb85b97257cfc
Majki94 <MMajkic123>
2020-03-06 21:55:06 +0100

    Search added.

--- app/src/main/java/com/majkic/mirko/mmdb/search/SearchFragment.java
+++ app/src/main/java/com/majkic/mirko/mmdb/search/SearchFragment.java
@@ -3,23 +3,45 @@ package com.majkic.mirko.mmdb.search;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.majkic.mirko.mmdb.R;
+import com.majkic.mirko.mmdb.adapters.MovieAdapter;
+import com.majkic.mirko.mmdb.adapters.RecentSearchesAdapter;
 import com.majkic.mirko.mmdb.model.Movie;
 import com.majkic.mirko.mmdb.model.Search;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 public class SearchFragment extends Fragment implements SearchContract.View {
 
     private SearchContract.UserActionsListener mPresenter;
     private Unbinder unbinder;
+
+    @BindView(R.id.search_edit_text)
+    EditText search;
+    @BindView(R.id.search_button)
+    FrameLayout searchButton;
+    @BindView(R.id.recent_searches)
+    RecyclerView recentSearchesList;
+    @BindView(R.id.search_results)
+    RecyclerView searchResultsList;
+    @BindView(R.id.progress)
+    ProgressBar progress;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -48,7 +70,33 @@ public class SearchFragment extends Fragment implements SearchContract.View {
         View root = inflater.inflate(R.layout.fragment_search, container, false);
         unbinder = ButterKnife.bind(this, root);
 
-        mPresenter = new SearchPresenter(this);
+        mPresenter = new SearchPresenter(getContext(), this);
+
+        recentSearchesList.setLayoutManager(new LinearLayoutManager(getContext()));
+        recentSearchesList.setAdapter(new RecentSearchesAdapter(getContext(), new ArrayList<Search>(), new RecentSearchesAdapter.ItemClickedListener() {
+            @Override
+            public void onSearchClicked(Search search) {
+                mPresenter.startSearch(search.getSearchTerm());
+            }
+        }));
+
+        searchResultsList.setLayoutManager(new LinearLayoutManager(getContext()));
+        searchResultsList.setAdapter(new MovieAdapter(getContext(), new ArrayList<Movie>(), new MovieAdapter.MovieClickListener() {
+            @Override
+            public void onMovieClicked(Movie m) {
+
+            }
+
+            @Override
+            public void onFavouriteClicked(Movie m) {
+
+            }
+
+            @Override
+            public void onWatchedClicked(Movie m) {
+
+            }
+        }));
 
         return root;
     }
@@ -71,12 +119,50 @@ public class SearchFragment extends Fragment implements SearchContract.View {
     }
 
     @Override
-    public void showRecentSearches(List<Search> searchList) {
+    public void onResume() {
+        super.onResume();
+        mPresenter.getRecentSearches();
+    }
 
+    @Override
+    public void showProgress() {
+        if (progress != null) {
+            progress.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void hideProgress() {
+        if (progress != null) {
+            progress.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void showRecentSearches(List<Search> searchList) {
+        if (searchResultsList != null && recentSearchesList != null && recentSearchesList.getAdapter() != null) {
+            searchResultsList.setVisibility(View.GONE);
+            recentSearchesList.setVisibility(View.VISIBLE);
+            ((RecentSearchesAdapter) recentSearchesList.getAdapter()).setSearchList(searchList);
+        }
     }
 
     @Override
     public void showSearchResults(List<Movie> searchResults) {
+        if (recentSearchesList != null && searchResultsList != null && searchResultsList.getAdapter() != null) {
+            recentSearchesList.setVisibility(View.GONE);
+            searchResultsList.setVisibility(View.VISIBLE);
+            ((MovieAdapter) searchResultsList.getAdapter()).setMovieList(searchResults);
+        }
+    }
 
+    @OnClick(R.id.search_button)
+    public void search() {
+        String termToSearch = search.getText().toString();
+        if (termToSearch.isEmpty()) {
+            Toast.makeText(getContext(), "Please input text to search", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mPresenter.startSearch(termToSearch);
     }
 }
--- app/src/main/java/com/majkic/mirko/mmdb/search/SearchPresenter.java
+++ app/src/main/java/com/majkic/mirko/mmdb/search/SearchPresenter.java
@@ -1,20 +1,52 @@
 package com.majkic.mirko.mmdb.search;
 
+import android.content.Context;
+
+import com.majkic.mirko.mmdb.data.SearchDataRepository;
+import com.majkic.mirko.mmdb.data.SearchDataRepositoryImplementation;
+import com.majkic.mirko.mmdb.model.Movie;
+import com.majkic.mirko.mmdb.model.Search;
+
+import java.util.List;
+
 public class SearchPresenter implements SearchContract.UserActionsListener {
 
+    private Context context;
     private SearchContract.View view;
+    private SearchDataRepository repository;
 
-    public SearchPresenter(SearchContract.View view) {
+    public SearchPresenter(Context context, SearchContract.View view) {
+        this.context = context;
         this.view = view;
+        repository = SearchDataRepositoryImplementation.getInstance(context);
     }
 
     @Override
     public void getRecentSearches() {
-
+        view.showProgress();
+        repository.getRecentSearches(new SearchDataRepository.RecentSearchesCallback() {
+            @Override
+            public void onRecentSearchesGot(List<Search> searches) {
+                view.showRecentSearches(searches);
+                view.hideProgress();
+            }
+        });
     }
 
     @Override
-    public void startSearch(String term) {
-
+    public void startSearch(final String term) {
+        view.showProgress();
+        repository.saveSearch(new Search(term, System.currentTimeMillis()), new SearchDataRepository.SaveSearchCallback() {
+            @Override
+            public void onSearchSaved() {
+                repository.startSearch(term, new SearchDataRepository.SearchCallback() {
+                    @Override
+                    public void onSearchResultsGot(List<Movie> foundMovies) {
+                        view.showSearchResults(foundMovies);
+                        view.hideProgress();
+                    }
+                });
+            }
+        });
     }
 }
